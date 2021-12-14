d1165507883030e60a2de4b15da9baa594aef7cd
masprog2022 <masprog2022@gmail.com>
2021-12-14 23:37:22 +0100

    integrar frontend com backend

---
+++ backend/src/routes/users.routes.ts
@@ -0,0 +1,62 @@
+import { Router } from 'express';
+
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
+
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import CreateUserService from '../services/CreateUserService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const usersRouter = Router();
+const upload = multer(uploadConfig);
+
+//usersRouter.use(ensureAuthenticated);
+
+// Rota POST
+usersRouter.post('/', async (request, response) => {
+
+
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+
+
+});
+
+// Rota PATCH
+usersRouter.patch('/avatar',ensureAuthenticated,
+upload.single('avatar'),
+async (request, response) => {
+
+
+  const updateUserAvatar = new UpdateUserAvatarService();
+  //const user
+
+  const user = await updateUserAvatar.execute({
+    user_id: request.user.id,
+    avatarFilename: request.file.filename,
+ });
+
+ delete user.password;
+
+  return response.json(user);
+
+
+
+  }
+);
+
+export default usersRouter;
---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () =>
+
+(
+  <>
+<h1>Hello word</h1>
+  <GlobalStyle />
+  </>
+)
+
+export default App;
