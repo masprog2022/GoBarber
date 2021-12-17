c983a457d859be2377ae314744343c995064dc11
masprog2022 <masprog2022@gmail.com>
2021-12-17 23:41:06 +0100

    utilizando unform

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -1,21 +1,41 @@
 //import React, { ButtonHTMLAttributes } from 'react';
 
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
 import { IconBaseProps } from 'react-icons';
 import { Container } from './styles';
+import { useField } from '@unform/core';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> { // essa interface faz com que podemos utilizar elementos HTML dentro do nosso input
   name: string;
   icon?: React.ComponentType<IconBaseProps>; // permite pasar como parametro um componente.
 }
 
-const Input: React.FC<InputProps> = ({icon: Icon, ...rest}) => ( // passamos o InputProps como parametro dentro do React.FC, assim podemos utilizar todos elementos HTML do nosso input
-  <Container>
-    { Icon && <Icon size={20} /> }
+const Input: React.FC<InputProps> = ({name, icon: Icon, ...rest}) => {
 
-    <input {...rest} />
-  </Container>
-) //  dentro do input passamos todos as propriedades utilizando o {...props} o famoso spread no React
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  useEffect(() => {
+     registerField({
+       name: fieldName,
+       ref: inputRef.current,
+       path: 'value'
+     })
+  }, [fieldName, registerField])
+
+  return ( // passamos o InputProps como parametro dentro do React.FC, assim podemos utilizar todos elementos HTML do nosso input
+    <Container>
+      { Icon && <Icon size={20} /> }
+
+      <input
+      defaultValue={defaultValue}
+      ref={inputRef}
+      {...rest}
+      />
+
+    </Container>
+  )
+} //  dentro do input passamos todos as propriedades utilizando o {...props} o famoso spread no React
 
 
 
--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,10 +1,10 @@
-//import React, { ButtonHTMLAttributes } from 'react';
-
 import React from 'react';
 
 import { Container, Content, Background } from './styles';
 
 import { FiMail, FiLock, FiArrowLeft, FiUser } from 'react-icons/fi';
+
+import {  Form } from '@unform/web';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -12,37 +12,44 @@ import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.svg';
 
+const SignUp: React.FC = () => {
 
+  function handleSubmit(data: Object): void {
+    console.log(data)
+  }
 
-const SignUp: React.FC = () =>
+  return (
 
-(
   <Container>
 
-    <Background />
-      <Content>
-          <img src={logoImg} alt="Gobarber" />
-          <form>
-            <h1>Faça seu cadastro </h1>
-            <Input name="name" icon={FiUser} placeholder="Nome" />
-            <Input name="email" icon={FiMail} placeholder="Email" />
-            <Input
-            name="password"
-            icon={FiLock} type="password"
-            placeholder="Senha"
-            />
+  <Background />
+    <Content>
+        <img src={logoImg} alt="Gobarber" />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro </h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="Email" />
+          <Input
+          name="password"
+          icon={FiLock} type="password"
+          placeholder="Senha"
+          />
 
-            <Button type="submit">Cadastrar</Button>
+          <Button type="submit">Cadastrar</Button>
 
-          </form>
+        </Form>
 
-          <a href="#">
-            <FiArrowLeft />
-            Voltar para logon
-            </a>
-      </Content>
+        <a href="#">
+          <FiArrowLeft />
+          Voltar para logon
+          </a>
+    </Content>
 
-  </Container>
-)
+</Container>
+
+  )
+}
+
+
 
 export default SignUp;
