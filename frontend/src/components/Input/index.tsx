d059c235b57474a89c97e63f9e36a51e5e844c7e
masprog2022 <masprog2022@gmail.com>
2021-12-18 00:17:18 +0100

    usablidade do input

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -1,6 +1,6 @@
 //import React, { ButtonHTMLAttributes } from 'react';
 
-import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
+import React, { InputHTMLAttributes, useEffect, useRef, useState, useCallback } from 'react';
 import { IconBaseProps } from 'react-icons';
 import { Container } from './styles';
 import { useField } from '@unform/core';
@@ -12,8 +12,21 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> { // essa int
 
 const Input: React.FC<InputProps> = ({name, icon: Icon, ...rest}) => {
 
-  const inputRef = useRef(null);
+  const inputRef = useRef<HTMLInputElement>(null);
   const { fieldName, defaultValue, error, registerField } = useField(name);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true)
+  }, [])
+
+  const handleInputBlur = useCallback(() => {
+
+    setIsFocused(false)
+    setIsFilled(!!inputRef.current?.value)
+
+  }, [])
 
   useEffect(() => {
      registerField({
@@ -23,11 +36,15 @@ const Input: React.FC<InputProps> = ({name, icon: Icon, ...rest}) => {
      })
   }, [fieldName, registerField])
 
+
+
   return ( // passamos o InputProps como parametro dentro do React.FC, assim podemos utilizar todos elementos HTML do nosso input
-    <Container>
+    <Container isFilled={isFilled} isFocused={isFocused}>
       { Icon && <Icon size={20} /> }
 
       <input
+      onFocus={handleInputFocus}
+      onBlur={handleInputBlur}
       defaultValue={defaultValue}
       ref={inputRef}
       {...rest}
--- frontend/src/components/Input/styles.ts
+++ frontend/src/components/Input/styles.ts
@@ -1,19 +1,37 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
 
  background: #232129;
     border-radius: 10px;
-    border: 1px solid #212329;
+
     padding: 16px;
     width: 100%;
     display: flex;
     align-items: center;
+
+    border: 1px solid #212329;
     color: #666360;
 
     & + div {
       margin-top: 8px;
     }
+
+    ${props => props.isFocused && css`
+       color: #ff9000;
+       border-color: #ff9000;
+` }
+
+   ${props => props.isFilled && css`
+    color: #ff9000;
+` }
+
+
 
 input {
   background: transparent;
