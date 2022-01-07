3d359398499705f66c2d4f799c620627709bd6aa
masprog2022 <masprog2022@gmail.com>
2022-01-07 17:45:18 +0100

    Reescrevendo Repositórios

--- backend/src/modules/appoitments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ backend/src/modules/appoitments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,15 +1,34 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 import IAppointmentsRepository from '@modules/appoitments/repositories/IAppointmentsRepository';
 import Appointment from '../entities/Appointment';
+import ICreateAppointmentDTO from '@modules/appoitments/dtos/ICreateAppointmentDTO';
 
-@EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment> implements IAppointmentsRepository {
+
+class AppointmentsRepository implements IAppointmentsRepository {
+
+   private ormRepository: Repository<Appointment>;
+
+   constructor (){
+
+      this.ormRepository = getRepository(Appointment);
+  }
+
   public async findByDate(date: Date): Promise<Appointment | null> {
-    const findAppointment = await this.findOne({
+    const findAppointment = await this.ormRepository.findOne({
       where: { date },
     });
     return findAppointment || null;
   }
+  public async create({provider_id, date }: ICreateAppointmentDTO): Promise<Appointment>{
+
+    const appointment = this.ormRepository.create({provider_id, date})
+
+    await this.ormRepository.save(appointment);
+
+    return appointment;
+  }
 }
+
+
 
 export default AppointmentsRepository;
